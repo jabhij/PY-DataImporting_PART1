a55f4d4bdea924c7d4b59734a39892097ccdff97
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-22 19:42:54 +0530

    Create ImportingText_LBL.py

---
+++ PART-1/Introduction_Flat_Files/ImportingText_LBL.py
@@ -0,0 +1,7 @@
+"""
+Instructions --
+
+- Open moby_dick.txt using the with context manager and the variable file.
+- Print the first three lines of the file to the shell by using readline() three times within the context manager.
+"""
+
