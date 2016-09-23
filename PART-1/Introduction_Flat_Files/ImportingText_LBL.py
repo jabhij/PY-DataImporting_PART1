f9918b3922f18bec12c86a8afb75b4d01e323978
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-23 19:53:00 +0530

    Importing text line by line

--- PART-1/Introduction_Flat_Files/ImportingText_LBL.py
+++ PART-1/Introduction_Flat_Files/ImportingText_LBL.py
@@ -5,3 +5,10 @@ Instructions --
 - Print the first three lines of the file to the shell by using readline() three times within the context manager.
 """
 
+# Read & print the first 3 lines
+with open('moby_dick.txt') as file:
+    
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
+    
