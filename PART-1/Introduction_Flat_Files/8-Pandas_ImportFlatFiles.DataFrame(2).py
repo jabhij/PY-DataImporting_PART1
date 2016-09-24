910455e746bf778035fc828ed812afe66d358e60
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-24 12:47:18 +0530

    Using Pandas to Import Flat Files as DataFrames (2)

---
+++ PART-1/Introduction_Flat_Files/8-Pandas_ImportFlatFiles.DataFrame(2).py
@@ -0,0 +1,9 @@
+"""
+Instructions --
+
+- Import the first 5 rows of the file into a DataFrame using the function pd.read_csv() and assign the result to data. You'll need to use 
+the arguments nrows and header (there is no header in this file).
+- Build a numpy array from the resulting DataFrame in data and assign to data_array.
+- Execute print(type(data_array)) to print the datatype of data_array.
+"""
+
