05c2667602ae29c6fd3536f61a99851767be05d6
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-26 16:06:36 +0530

    The Structure of .mat in Python

---
+++ PART-1/Introduction_Other_FileType/10-Structure_MATLAB_File.py
@@ -0,0 +1,30 @@
+"""
+Instructions --
+
+- Use the method .keys() on the dictionary mat to print the keys. Most of these keys (in fact the ones that do NOT begin and end with '__')
+are variables from the corresponding MATLAB environment.
+- Print the type of the value corresponding to the key 'CYratioCyt' in mat. Recall that mat['CYratioCyt'] accesses the value.
+- Print the shape of the value corresponding to the key 'CYratioCyt' using the numpy function shape().
+- Execute the entire script to see some oscillatory gene expression data!
+"""
+
+# Print the keys of the MATLAB dictionary
+print(mat.keys())
+
+# Print the type of the value corresponding to the key 'CYratioCyt'
+print(type(mat['CYratioCyt']))
+
+# Print the shape of the value corresponding to the key 'CYratioCyt'
+print(np.shape(mat['CYratioCyt']))
+
+# Subset the array and plot it
+data = mat['CYratioCyt'][25, 5:]
+fig = plt.figure()
+plt.plot(data)
+plt.xlabel('time (min.)')
+plt.ylabel('normalized fluorescence (measure of expression)')
+plt.show()
+
+"""
+PLOT 4
+"""
