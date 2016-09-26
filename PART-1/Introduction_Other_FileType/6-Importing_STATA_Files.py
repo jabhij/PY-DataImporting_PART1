c51cfce8dff64a933ada73d63ca97569e56c92d7
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-26 15:37:49 +0530

    Importing STATA Files

---
+++ PART-1/Introduction_Other_FileType/6-Importing_STATA_Files.py
@@ -0,0 +1,26 @@
+"""
+Instructions --
+
+- Use pd.read_stata() to load the file 'disarea.dta' into the DataFrame df.
+- Print the head of the DataFrame df.
+- Visualize your results by plotting a histogram of the column disa10. We’ve already provided this code for you, so just run it!
+"""
+
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata('disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of coutries')
+plt.show()
+
+"""
+PLOT 2
+"""
