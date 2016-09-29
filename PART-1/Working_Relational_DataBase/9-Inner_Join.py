ee8252c3586feca3bada034ce7dd485504b38902
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-29 13:07:32 +0530

    The Power of SQL Lies in Relationships Between Tables: INNER JOIN

--- PART-1/Working_Relational_DataBase/9-Inner_Join.py
+++ PART-1/Working_Relational_DataBase/9-Inner_Join.py
@@ -7,3 +7,12 @@ each record from the Album table and the Artist table, respectively. To do so, I
 - Set the DataFrame's column names to the corresponding names of the table columns.
 """
 
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute('SELECT Title, Name from Album INNER JOIN Artist on Album.ArtistID = Artist.ArtistID')
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print head of DataFrame df
+print(df.head())
