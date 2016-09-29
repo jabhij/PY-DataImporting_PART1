5ebf23c931160271421c88738127b8a0f9692931
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-29 12:41:13 +0530

    The power of SQL lies in relationships between tables: INNER JOIN

---
+++ PART-1/Working_Relational_DataBase/9-Inner_Join.py
@@ -0,0 +1,9 @@
+"""
+Instructions --
+
+- Assign to rs the results from the following query: select all the records, extracting the Title of the record and Name of the artist of 
+each record from the Album table and the Artist table, respectively. To do so, INNER JOIN these two tables on the ArtistID column of both.
+- In a call to pd.DataFrame(), apply the method fetchall() to rs in order to fetch all records in rs. Store them in the DataFrame df.
+- Set the DataFrame's column names to the corresponding names of the table columns.
+"""
+
