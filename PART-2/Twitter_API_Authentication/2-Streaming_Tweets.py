6efece23169814d7240602e714bf0304aca2f6d3
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-10-05 17:43:45 +0530

    Streaming Tweets

--- PART-2/Twitter_API_Authentication/2-Streaming_Tweets.py
+++ PART-2/Twitter_API_Authentication/2-Streaming_Tweets.py
@@ -5,3 +5,13 @@ Instructions --
 - To filter Twitter Streams, pass to the track argument in stream.filter() a list containing the desired keywords 'clinton', 'trump', 
 'sanders', and 'cruz'.
 """
+
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create you Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(track = ['clinton', 'trump', 'sanders', 'cruz'])
